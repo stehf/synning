d0fe9416bc573469f4bb4deb1cd98eedb6904b18
stefftw <10836668+stefftw@users.noreply.github.com>
2021-09-10 00:46:17 -0400

    create vue app, add weekday config v1, add simple template for weekdays

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <Weekdays />
+  </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  import Weekdays from './components/Weekdays.vue'
+   export default {
+    name: 'app',
+    components: {
+      Weekdays
+    }
   }
-}
 </script>
-
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  body {
+    margin:  0;
+  }
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+  }
 </style>
---
+++ src/components/Weekdays.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="weekday">
+    <h3>{{todayName}}</h3>
+  </div>
+</template>
+<script>
+  import days from "./json/color-map.json"
+
+  // get today's hex code
+  const date = new Date();
+  const today = date.getDay();
+  const todayColor = days[today].hexCode || "#000";
+  const todayName = days[today].name || "Happy Birthday!";
+
+  export default {
+    name: 'Weekdays',
+    data() {
+      return {
+        todayColor: todayColor,
+        todayName: todayName
+      }
+    }
+  }
+</script>
+<style>
+  h3 {
+    color:  white;
+    margin:  0;
+  }
+  .weekday {
+    background-color: v-bind(todayColor);
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+</style>
